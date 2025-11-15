43cede4a61d2b0598e25ca1009ac6ab09f8bf329
April <liapandeiroth34190@gmail.com.com>
2025-11-15 21:24:33 +0800

    tambah eslint + prettier + konfigurasi.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <h1>FE-AMANAT Starter</h1>
       <p>Halo, ini starter React (Vite).</p>
     </div>
