22a42bc36f01efa79fd5d7abff2ad70e85b35db7
April <liapandeiroth34190@gmail.com.com>
2025-11-15 20:48:36 +0800

    Starter Project React

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='app'>
+      <h1>FE-AMANAT Starter</h1>
+      <p>Halo, ini starter React (Vite).</p>
+    </div>
+  );
+}
