04d11fe3e8a82f16c10b328024a4837d9a1dfb29
April <liapandeiroth34190@gmail.com.com>
2025-11-16 21:41:16 +0800

    chore: reset to React starter (clean src, add main.jsx & index.css)

--- src/lib/utils.js
+++
@@ -1,8 +0,0 @@
-export function formatDate(dateStr) {
-  const d = new Date(dateStr);
-  return d.toLocaleDateString('id-ID');
-}
-
-export function cn(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
